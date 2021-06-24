dee9faf0dd3804d537a7d881e89e65a0d5010a46
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-06-24 10:28:01 +0900

    post serviceを作成 responseはまだ作ってない

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -27,19 +27,32 @@ func newGrpcClientConn() *grpc.ClientConn {
 func request() {
 	router := gin.Default()
 	conn := newGrpcClientConn()
+
 	userClient := pb.NewUserServiceClient(conn)
-	client := NewClient(userClient)
+	postClient := pb.NewPostServiceClient(conn)
+	client := NewClient(
+		userClient,
+		postClient,
+	)
+
 	router.GET("/users", client.GetUsers)
+	router.GET("/posts", client.GetPosts)
+
 	router.Run(":" + os.Getenv("PORT"))
 }
 
 type client struct {
 	user pb.UserServiceClient
+	post pb.PostServiceClient
 }
 
-func NewClient(userClient pb.UserServiceClient) *client {
+func NewClient(
+	userClient pb.UserServiceClient,
+	postClient pb.PostServiceClient,
+) *client {
 	return &client{
 		user: userClient,
+		post: postClient,
 	}
 }
 
@@ -52,4 +65,15 @@ func (c *client) GetUsers(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, users)
+}
+
+func (c *client) GetPosts(ctx *gin.Context) {
+	posts, err := c.post.GetPosts(context.Background(), &emptypb.Empty{})
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"errorMessage": string(err.Error()),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, posts)
 }
