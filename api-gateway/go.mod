b20de3a60011c63585f2bd20d26b20ac8aae37eb
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-06-26 06:58:42 +0900

    change dir

--- api-gateway/go.mod
+++
@@ -1,9 +0,0 @@
-module app
-
-go 1.16
-
-require (
-	github.com/gin-gonic/gin v1.7.2
-	google.golang.org/grpc v1.38.0
-	google.golang.org/protobuf v1.26.0
-)
---
+++ services/api-gateway/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"app/pb"
+	"context"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func main() {
+	request()
+}
+
+func newGrpcClientConn() *grpc.ClientConn {
+	targetUrl := os.Getenv("USER_SERVER_NAME") + ":" + os.Getenv("GRPC_SERVER_PORT")
+	conn, err := grpc.Dial(targetUrl, grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	return conn
+}
+
+func request() {
+	router := gin.Default()
+	conn := newGrpcClientConn()
+
+	userClient := pb.NewUserServiceClient(conn)
+	postClient := pb.NewPostServiceClient(conn)
+	client := NewClient(
+		userClient,
+		postClient,
+	)
+
+	router.GET("/users", client.GetUsers)
+	router.GET("/posts", client.GetPosts)
+
+	router.Run(":" + os.Getenv("PORT"))
+}
+
+type client struct {
+	user pb.UserServiceClient
+	post pb.PostServiceClient
+}
+
+func NewClient(
+	userClient pb.UserServiceClient,
+	postClient pb.PostServiceClient,
+) *client {
+	return &client{
+		user: userClient,
+		post: postClient,
+	}
+}
+
+func (c *client) GetUsers(ctx *gin.Context) {
+	users, err := c.user.GetUsers(context.Background(), &emptypb.Empty{})
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"errorMessage": string(err.Error()),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, users)
+}
+
+func (c *client) GetPosts(ctx *gin.Context) {
+	posts, err := c.post.GetPosts(context.Background(), &emptypb.Empty{})
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"errorMessage": string(err.Error()),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, posts)
+}
