4dd3cbb8ba1754f98a3478cb702e3f64dd263d82
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-06-26 10:44:49 +0900

    fix port & converter

--- services/user/go.mod
+++ services/user/go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/golang/protobuf v1.5.0
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/uber/jaeger-client-go v2.29.1+incompatible // indirect
--- services/user/interface/converter/user.go
+++ services/user/interface/converter/user.go
@@ -10,13 +10,13 @@ import (
 func ConvertUsers(users []*domain.User) (*pb.GetUsersResponse, error) {
 	var pbUsers []*pb.User
 	for _, user := range users {
-		pbUser, _ := ConvertUser(user)
+		pbUser := ConvertUser(user)
 		pbUsers = append(pbUsers, pbUser)
 	}
 	return &pb.GetUsersResponse{Users: pbUsers}, nil
 }
 
-func ConvertUser(user *domain.User) (*pb.User, error) {
+func ConvertUser(user *domain.User) *pb.User {
 	pbUser := &pb.User{
 		Id:              user.Id,
 		LastName:        user.LastName,
@@ -24,8 +24,9 @@ func ConvertUser(user *domain.User) (*pb.User, error) {
 		Email:           user.Email,
 		TelephoneNumber: user.TelephoneNumber,
 		Gender:          uint64(user.Gender),
-		CreatedAt:       timestamppb.New(user.CreatedAt),
-		UpdatedAt:       timestamppb.New(user.UpdatedAt),
+		// clientがRailsだとRFC3339に変えるけど、clientがGoだとepoch timeで返してる
+		CreatedAt: timestamppb.New(user.CreatedAt),
+		UpdatedAt: timestamppb.New(user.UpdatedAt),
 	}
-	return pbUser, nil
+	return pbUser
 }
